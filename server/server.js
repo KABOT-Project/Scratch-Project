120e0983653203e8fdc24c54d917ed9765b14cc2
Thomas Pappas <thomaskpappas@gmail.com>
2023-07-05 21:49:07 -0400

    first file uploads to main

---
+++ server/server.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
+const router = require('./routes/routes');
+
+const PORT = 3000;
+
+const app = express();
+
+const mongoURI = 'mongodb+srv://thomaskpappas:9TbWXn8GlVgb7ffk@soloprojectcluster.m9jqswl.mongodb.net/?retryWrites=true&w=majority';
+
+// connect to MongoDB and listen on port 3000
+mongoose.connect(mongoURI, err => {
+  if (err) {
+    console.error('MongoDB connection error:', err);
+  } else {
+    console.log('Connected to MongoDB Atlas');
+    app.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  }
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// setup session functionality 
+app.use(
+  session({
+    secret: 'tkpaps', 
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: mongoURI, 
+    }),
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24, 
+    },
+  })
+);
+
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  next();
+});
+
+// serve static pages
+app.use('/client', express.static(path.resolve(__dirname, '../client')));
+
+// use all routes in routes folder
+app.use('/api', router);
+
+// catch-all route handler for any requests to an unknown route
+app.use((req, res) => res.status(404).send('404 page not found'));
+
+// global error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+
+module.exports = app;
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/index.html',
+    }),
+  ],
+  resolve: {
+    modules: [__dirname, 'client', 'node_modules'],
+    extensions: ['.*', '.js', '.jsx', '.tsx', '.ts'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: require.resolve('babel-loader'),
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.png|svg|jpg|gif$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  devtool: 'eval-source-map',
+  mode: process.env.NODE_ENV,
+  devServer: {
+    compress: true,
+    port: 8080,
+    historyApiFallback: true, // Enable history API fallback for React Router
+    host: 'localhost',
+    hot: true,
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    proxy: {
+      '/api/**': {
+        target: 'HTTP://localhost:3000/',
+        secure: false
+      },
+      '/assets/**': {
+        target: 'HTTP://localhost:3000/',
+        secure: false
+      }
+    }
+  },
+
+};
