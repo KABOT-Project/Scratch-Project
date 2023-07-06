81d3baf57bceb170c9921f17134b118123d2c6bb
Bryan Luna <luna.bryanr@gmail.com>
2023-07-05 23:00:10 -0400

    react pages added

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
-// import Homepage from './pages/Homepage.jsx';
-// import Login from './pages/Login.jsx';
-// import Signup from './pages/Signup.jsx';
-// import Dashboard from './pages/Dashboard.jsx';
+import Homepage from './pages/Homepage.jsx';
+import Login from './pages/Login.jsx';
+import Signup from './pages/Signup.jsx';
+import Dashboard from './pages/Dashboard.jsx';
+import Recipes from './pages/Recipes.jsx';
+import Add from './pages/Add.jsx';
 
 const App = () => {
   return (
@@ -14,6 +16,8 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/recipes" element={<Recipes />} />
+        <Route path="/add" element={<Add />} />
       </Routes>
     </>
   );
